69381d9240b204b5339e1c836d2de07e5ec8cde8
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2019-11-05 18:50:55 +0900

    저장기능 프로토 타입

---
+++ storage_test/storage.js
@@ -0,0 +1,39 @@
+var textarea_html_array=new Array()
+
+var st_button = document.createElement('button');
+st_button.innerHTML=("storage");
+st_button.style="position: fixed; top: 5px; left: 400px;";
+document.body.appendChild(st_button);
+st_button.addEventListener('click', createURL);
+
+var rc_button = document.createElement('button');
+rc_button.innerHTML=("recieve");
+rc_button.style="position: fixed; top: 30px; left: 400px;";
+document.body.appendChild(rc_button);
+rc_button.addEventListener('click', recieveURL);
+
+var space=document.createElement('p')
+space.id="Element";
+document.body.appendChild(space);
+
+
+
+function recieveURL(){
+	chrome.storage.sync.get("data", function(items) {
+		if (!chrome.runtime.error) {
+	    	document.getElementById('Element').innerHTML=items.data;
+		}
+	});
+}
+
+function createURL() {
+	for(var i=0 in textarea_array){
+		textarea_html_array[i]=textarea_array[i].outerHTML;
+	}
+    chrome.storage.sync.set({ "data" : textarea_html_array }, function() {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+    });
+}
+
