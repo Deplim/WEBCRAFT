885d519eeafea361b6f34c627dd8e8c8308660c8
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2019-11-13 19:52:47 +0900

    Add loadTextArea delete
    로드한 텍스트 에어리어도 x버튼 눌러 삭제 가능하게 함

--- borwser_editor_code/storage.js
+++ borwser_editor_code/storage.js
@@ -26,17 +26,14 @@ document.body.appendChild(space);
 function recieveURL(){
 	chrome.storage.sync.get("data", function(items) {
 		if (!chrome.runtime.error) {
-	    	document.getElementById('Element').innerHTML=items.data;
+	    	// document.getElementById('Element').innerHTML= items.data;
+			document.body.innerHTML = document.body.innerHTML + items.data;
 
 			ta_delete_List=document.getElementsByClassName("ta_delete_class");
 
-			ta_delete_List[0].addEventListener("click", delete_text2);
-			// ta_delete_List[1].addEventListener("click", delete_textarea);
-			console.log(ta_delete_List[0].parentNode.parentNode);
-			// for(var i =0; i<ta_delete_List.length; i++){
-			// 	ta_delete_List[0].addEventListener("click", delete_textarea);
-			// 	console.log(ta_delete_List[i].parentNode);
-			// }
+			for(var i =0; i<ta_delete_List.length; i++){
+				ta_delete_List[i].addEventListener("click", delete_textarea);
+			}
 		}
 	});
 }
@@ -56,9 +53,5 @@ function createURL() {
             console.log("Runtime error.");
         }
     });
-}
-function delete_text2(e) {
-	var node = (e || event).target;
-	document.getElementById("Element").removeChild(node.parentNode);
 }
 
