baa9a7cd5066e795dde5553e5da0b9f4674fb1af
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2019-11-17 21:18:48 +0900

    Change capture.js 공백
    capture.js 공백 수정

--- borwser_editor_code/capture.js
+++ borwser_editor_code/capture.js
@@ -22,150 +22,133 @@ $("#captureButton").on('click', function(e) {  //캡쳐 기능 활성화
 
   $("body").append($focus);  //마우스 커서에 따라 캡쳐 영역을 만들 div
 
-
-
   var selectArea = false;
 
   $("body").one("mousedown", function(e) {  //캡쳐 영역 선택 시작
 
-    e.preventDefault();
+      e.preventDefault();
 
-    selectArea = true;
+      selectArea = true;
 
-    startX = e.clientX;
+      startX = e.clientX;
 
-    startY = e.clientY;
+      startY = e.clientY;
 
   }).one('mouseup', function(e) {  //캡쳐 시작
 
-    selectArea = false;
+      selectArea = false;
+
+      $("body").off('mousemove', mousemove);  //이벤트 삭제
+
+      $("#screenshot_focus").remove();  //마우스 포커스 삭제
+
+      $("#screenshot_mask").remove();  //딤 삭제
 
 
+      var x = e.clientX; // 상대 좌표
+      var y = e.clientY;
+      var top = Math.min(y, startY); // 상대 좌표 기준 위
+      var left = Math.min(x, startX); // 상대 좌표 기준 왼쪽
+      var width = Math.max(x, startX) - left; // 가로 길이
+      var height = Math.max(y, startY) - top; // 세로 길이
 
-    $("body").off('mousemove', mousemove);  //이벤트 삭제
+      top = top + window.pageYOffset;
 
-    $("#screenshot_focus").remove();  //마우스 포커스 삭제
+      html2canvas(document.body).then(function(canvas) {  //전체 화면 캡쳐
 
-    $("#screenshot_mask").remove();  //딤 삭제
+          var img = canvas.getContext('2d').getImageData(left, top, width, height);  //선택 영역만큼 crop
+          console.log(left);
+          console.log(top);
+          console.log(width);
+          console.log(height);
+          var c = document.createElement("canvas");
 
+          c.width = width;
+          c.height = height;
 
+          c.getContext('2d').putImageData(img, 0, 0);
 
-    var x = e.clientX;
-
-    var y = e.clientY;
-
-    var top = Math.min(y, startY);
-
-    var left = Math.min(x, startX);
-
-    var width = Math.max(x, startX) - left;
-
-    var height = Math.max(y, startY) - top;
-
-
-    html2canvas(document.body).then(function(canvas) {  //전체 화면 캡쳐
-
-      var img = canvas.getContext('2d').getImageData(left, top, width, height);  //선택 영역만큼 crop
-
-      var c = document.createElement("canvas");
-
-      c.width = width;
-
-      c.height = height;
-
-      c.getContext('2d').putImageData(img, 0, 0);
-
-      save(c);  //crop한 이미지 저장
-
-    });
-
+          save(c);  //crop한 이미지 저장
+      });
   }).on("mousemove", mousemove);  //캡쳐 영역 크기 변경
-
-
 
   function mousemove(e) {
 
-    var x = e.clientX;
+      var x = e.clientX;
+      var y = e.clientY;
 
-    var y = e.clientY;
+      $focus.css("left", x);  //마우스 커서 따라 좌표 포커스 이동
+      $focus.css("top", y);
 
-    $focus.css("left", x);  //마우스 커서 따라 좌표 포커스 이동
+      if(selectArea) {  //캡쳐 영역 선택 그림
 
-    $focus.css("top", y);
+          var top = Math.min(y, startY);
 
+          var right = width - Math.max(x, startX);
 
+          var bottom = height - Math.max(y, startY);
 
-    if(selectArea) {  //캡쳐 영역 선택 그림
+          var left = Math.min(x, startX);
 
-      var top = Math.min(y, startY);
-
-      var right = width - Math.max(x, startX);
-
-      var bottom = height - Math.max(y, startY);
-
-      var left = Math.min(x, startX);
-
-      $mask.css("border-width", [top + 'px', right + 'px', bottom + 'px', left + 'px'].join(' '));
-
-    }
-
+          $mask.css("border-width", [top + 'px', right + 'px', bottom + 'px', left + 'px'].join(' '));
+      }
   }
 
 
 
   function save(canvas) {  //파일로 저장
 
-    if (navigator.msSaveBlob) {
+      if (navigator.msSaveBlob) {
 
-      var blob = canvas.msToBlob(); 
+          var blob = canvas.msToBlob();
 
-      return navigator.msSaveBlob(blob, '파일명.jpg'); 
+          return navigator.msSaveBlob(blob, '파일명.jpg');
 
-    } else { 
-      var capture = document.createElement('img');  
-      capture.style="border: 1px solid blue; position: fixed; top: 0px; left: 0px;";
-      capture.class="capture";
-      capture.setAttribute('draggable', true);
-      capture.src = canvas.toDataURL("image/jpeg");
-  	  capture.onmousedown = function(event) {
-		  let shiftX = event.clientX - capture.getBoundingClientRect().left;
-		  let shiftY = event.clientY - capture.getBoundingClientRect().top;
+      } else {
+          var capture = document.createElement('img');
+          capture.style="border: 1px solid blue; position: fixed; top: 0px; left: 0px;";
+          capture.class="capture";
+          capture.setAttribute('draggable', true);
+          capture.src = canvas.toDataURL("image/jpeg");
+          capture.onmousedown = function(event) {
+              let shiftX = event.clientX - capture.getBoundingClientRect().left;
+              let shiftY = event.clientY - capture.getBoundingClientRect().top;
 
-		  capture.style.position = 'absolute';
-		  capture.style.zIndex = 1000;
-		  document.body.append(capture);
+              capture.style.position = 'absolute';
+              capture.style.zIndex = 1000;
+              document.body.append(capture);
 
-		  moveAt(event.pageX, event.pageY);
+              moveAt(event.pageX, event.pageY);
 
 		  // moves the ball at (pageX, pageY) coordinates
 		  // taking initial shifts into account
-		  function moveAt(pageX, pageY) {
-		    capture.style.left = pageX - shiftX + 'px';
-		    capture.style.top = pageY - shiftY + 'px';
-		  }
+              function moveAt(pageX, pageY) {
+                  capture.style.left = pageX - shiftX + 'px';
+                  capture.style.top = pageY - shiftY + 'px';
+              }
 
-		  function onMouseMove(event) {
-		    moveAt(event.pageX, event.pageY);
-		  }
+              function onMouseMove(event) {
+                  moveAt(event.pageX, event.pageY);
+              }
+
 
 		  // move the ball on mousemove
-		  document.addEventListener('mousemove', onMouseMove);
+              document.addEventListener('mousemove', onMouseMove);
 
 		  // drop the ball, remove unneeded handlers
-		  capture.onmouseup = function() {
-		    document.removeEventListener('mousemove', onMouseMove);
-		    capture.onmouseup = null;
-		  };
-      };
+              capture.onmouseup = function() {
+                  document.removeEventListener('mousemove', onMouseMove);
+                  capture.onmouseup = null;
+              };
+          };
 
-	  capture.ondragstart = function() {
-	     return false;
-      };
-      document.body.appendChild(capture);
-    }
+          capture.ondragstart = function() {
+              return false;
+          };
+          document.body.appendChild(capture);
+      }
 
   }
-
 });
 
 
