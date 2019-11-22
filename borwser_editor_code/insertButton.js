9c5f7a0c7966746c60d5554f9f0106a1f8e936e6
gusgh9176 <gusgh9176@naver.com>
2019-11-22 16:08:19 +0900

    Add onOff.js

--- borwser_editor_code/insertButton.js
+++ borwser_editor_code/insertButton.js
@@ -16,20 +16,25 @@ var ta_button = document.createElement('button');
 ta_button.id="textButton";
 ta_button.innerHTML=("textarea_mode(off)");
 
+var on_button = document.createElement('button');
+on_button.id="stateButton";
+on_button.innerHTML=("on");
 
 // style
 st_button.style="position: fixed; bottom: 40px; right: 10px;";
 rc_button.style="position: fixed; bottom: 10px; right: 10px;";
 ta_button.style="position: fixed; bottom: 10px; right: 170px;";
-
+on_button.style="position: fixed; bottom: 40px; right: 95px;";
 
 // body.appendChild
 document.body.appendChild(st_button);
 document.body.appendChild(rc_button);
 document.body.appendChild(ta_button);
+document.body.appendChild(on_button);
 
 
 // addEventListener
 st_button.addEventListener('click', createURL);
 rc_button.addEventListener('click', recieveURL);
 ta_button.addEventListener('click', text_area_mode);
+on_button.addEventListener('click', changeState);
---
+++ borwser_editor_code/onOff.js
@@ -0,0 +1,49 @@
+// 버튼 클릭시 확장프로그램의 기능을 on / off 함
+
+var state = true;
+
+function changeState() {
+    state = !state;
+    if(state){ // 관련 버튼, 작업물 보이게 함
+        on_button.innerHTML="on"; // 버튼 글자 변경
+        st_button.style.visibility="visible";
+        rc_button.style.visibility="visible";
+        ta_button.style.visibility="visible";
+        cp_button.style.visibility="visible";
+        document.getElementById("caret_id").style.visibility="visible";
+        // 클래스 읽어 와서 보이게 만듬
+        var div_area_List=document.getElementsByClassName("div_area_class");
+        for(var i = 0; i<div_area_List.length; i++){
+            div_area_List[i].style.visibility="visible";
+        }
+        var highlight_List = document.getElementsByClassName("highlight_class");
+        for(var i = 0; i<highlight_List.length; i++){
+            highlight_List[i].style.visibility="visible";
+        }
+        var jbBook_List = document.getElementsByClassName("jbBook_class");
+        for(var i = 0; i<jbBook_List.length; i++) {
+            jbBook_List[i].style.visibility = "visible";
+        }
+    }
+    else{ // 관련 버튼, 작업물 안보이게함
+        on_button.innerHTML="off";
+        st_button.style.visibility="hidden";
+        rc_button.style.visibility="hidden";
+        ta_button.style.visibility="hidden";
+        cp_button.style.visibility="hidden";
+        document.getElementById("caret_id").style.visibility="hidden";
+        var div_area_List=document.getElementsByClassName("div_area_class");
+        for(var i = 0; i<div_area_List.length; i++){
+            div_area_List[i].style.visibility="hidden";
+        }
+        var highlight_List = document.getElementsByClassName("highlight_class");
+        for(var i = 0; i<highlight_List.length; i++){
+            highlight_List[i].style.visibility="hidden";
+        }
+        var jbBook_List = document.getElementsByClassName("jbBook_class");
+        for(var i = 0; i<jbBook_List.length; i++) {
+            jbBook_List[i].style.visibility = "hidden";
+        }
+    }
+    update(); // In documentListener.js 업데이트
+}
